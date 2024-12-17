c79859f08645e8fad7562a77376b1caae7bc7142
mohamedsotohy <mo.hassan.9111@gmail.com>
2024-12-17 21:20:27 +0200

    feat: create login page basic components

---
+++ src/Logo.jsx
@@ -0,0 +1,17 @@
+import logo from "./images/quickwrench-logo.png";
+import "./Logo.css";
+export default function Logo()
+{
+
+    return(
+        <>
+        <div className={"panal"}>
+        <div className={"mylogo"}>
+        <img src={logo} alt="our logo" className={"image"}/>
+        
+      </div>
+      <h2 style={{width:"40%", fontSize:"40px"}}>QuickWrench</h2>
+      </div>
+      </>
+    );
+}
