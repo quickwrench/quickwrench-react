5bce83ae923a92e7fa0ab197d8408855adb099e8
mohamedsotohy <mo.hassan.9111@gmail.com>
2024-12-20 17:51:30 +0200

    feat:added the login form to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./Logo.jsx"
 import "./App.css";
 import Logo from "./Logo.jsx";
+import Login_form from "./login_form.jsx";
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Logo/>
-      
+      <Login_form/>
     </>
   );
 }
