cec1bf2a4598417c9e9a76f5c6e09887aba10697
mohamedsotohy <mo.hassan.9111@gmail.com>
2024-12-20 23:14:09 +0200

    chore: adding routes and changing the code format

---
+++ src/LoginForm.jsx
@@ -0,0 +1,37 @@
+import "./loginBtn.css";
+export default function Login_form() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className="login"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "30%",
+            width: "40%",
+            justifySelf: "center",
+            alignSelf: "center",
+          }}
+        >
+          <form
+            style={{
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <input type="text" placeholder="USERNAME" />
+            <input type="password" placeholder="PASSWORD" />
+            <button>Login</button>
+          </form>
+          <div style={{ marginTop: "10px" }}>
+            <span>don`t have an account? </span>
+            <a href=""> sign up</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
